4fcbc2d49d5d74c766f5bd0222e5652ece925bbf
Alexandre Lejal <alexandrelejal01@gmail.com>
2025-10-26 20:16:30 +0100

    Initialisation 2

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import * as react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react.default(), tsconfigPaths()],
   server: { port: 5173 },
 });
